01592f9f9220834565c5f9ec535b58b6b019e91c
Manuel Alabor <manuel@alabor.me>
2015-10-27 15:28:17 +0100

    implemented request flow for getting application resource

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var config = require('./config.json');
 var auth = require('./lib/auth');
+var autoDiscover = require('./lib/autoDiscover');
+var application = require('./lib/application');
 
 var autoDiscoverUrl = config.autoDiscoverUrl;
 var username = config.username;
@@ -7,8 +9,16 @@ var password = config.password;
 
 auth(autoDiscoverUrl, username, password)
 	.then(function(token) {
-		console.log('got token', token);
+		return autoDiscover(autoDiscoverUrl, 'user')
+			.then(function(userUrl) { return [ token, userUrl ]; })
+			.spread(application)
+			.then(function(application) {
+				console.log(application);
+			})
 	})
 	.catch(function(err) {
-		console.log('error :(', err);
+		console.log('error :(');
 	});
+
+
+
